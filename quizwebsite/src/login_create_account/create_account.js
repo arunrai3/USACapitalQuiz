4aeaeae9f66d963e55646fdd7c2e3d5ea874dcf9
Arun Rai <arunrai202@gmail.com>
2023-12-23 11:22:16 -0800

    added file to gitignore and creating POST request to pass user data from frontend to backend.

--- quizwebsite/src/login_create_account/create_account.js
+++ quizwebsite/src/login_create_account/create_account.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import styles from './create_account.module.css';
 
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const userData = {
+    email: 'user@example.com',
+    password: 'password123'
+  };
+
+  try {
+    const response = await fetch('http://localhost:8080/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    });
+
+    if (response.ok) {
+      console.log('User created successfully');
+    } else {
+      console.log('User not created successfully');
+    }
+  } catch (error) {
+    console.log('User not created successfully2');
+  }
+}
+
+
 function CreateAccount() {
   return (
     <div className={styles.loginContainer}>
@@ -8,7 +36,7 @@ function CreateAccount() {
       <form className={styles.loginForm}>
         <input type="email" placeholder="Email" className={styles.inputField} />
         <input type="password" placeholder="Password" className={styles.inputField} />
-        <button type="submit" className={styles.loginButton}>Cretae Account</button>
+        <button type="submit" className={styles.loginButton} onClick={handleSubmit}>Cretae Account</button>
         <p className={styles.signupText}>
           Already have an account? <a href="/login">Click here</a>
         </p>
